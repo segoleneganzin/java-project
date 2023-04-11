80442238fb8d0e031dc7e379e2a89ea9eb8d2631
lldel59 <lionel.delaby@wanadoo.fr>
2023-04-11 23:06:51 +0200

    J'arrète, je vais me coucher :)

--- PiscineM/src/main/java/application/PinCodeController.java
+++ PiscineM/src/main/java/application/PinCodeController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Button;
 import piscine.Main;
 
 public class PinCodeController {
+	//TODO Ajouter lorsque clique des touches 0 à 9, remplir le fx:id:pincode du PinCode.fxml et, si clique sur # (diese) valider, mais au préalable, vérifier si le pincode entré contient bien 4 chiffres.
 	   @FXML
 	    private Button home;
 
